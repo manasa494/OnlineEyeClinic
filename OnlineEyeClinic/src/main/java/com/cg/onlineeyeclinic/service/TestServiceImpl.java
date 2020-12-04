8783aab57ad9e787092c44d78fa8300c333a4ed1
manasa494 <75251411+manasa494@users.noreply.github.com>
2020-12-04 19:33:16 +0530

    Update TestServiceImpl.java

--- OnlineEyeClinic/src/main/java/com/cg/onlineeyeclinic/service/TestServiceImpl.java
+++ OnlineEyeClinic/src/main/java/com/cg/onlineeyeclinic/service/TestServiceImpl.java
@@ -3,6 +3,8 @@ package com.cg.onlineeyeclinic.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,71 +12,49 @@ import com.cg.onlineeyeclinic.exception.TestIdNotFoundException;
 import com.cg.onlineeyeclinic.model.Tests;
 import com.cg.onlineeyeclinic.repository.ITestRepository;
 
-@Service("name= TestService")
+@Service("name= testService")
 @Transactional
 public class TestServiceImpl implements ITestService {
-	
+
+	private static final String ERROR_MESSAGE = "Test ID Not Found";
 
 	@Autowired
-	ITestRepository repository;
+	private ITestRepository testRepository;
 
 	@Override
 	public Tests addTest(Tests tests) {
-		tests =  repository.save(tests);
-		return tests;
+		return testRepository.save(tests);
 	}
 
 	@Override
 	public Tests updateTest(Tests tests) {
-		tests =  repository.save(tests);
-		return tests;
-	}
-
-	public void deleteById(int testId) {
-		repository.deleteById(testId);
+		Integer testId = tests.getTestId();
+		Optional<Tests> test = testRepository.findById(testId);
+		if (test.isEmpty())
+			throw new TestIdNotFoundException(ERROR_MESSAGE);
+		return testRepository.save(tests);
 	}
 
 	@Override
-	public Tests deleteTest(int testId) throws TestIdNotFoundException{
-		<Optional<Tests> tes=repository.findById(id);
-		if(tes==null)
-			throw new TestIdNotFoundException("test id not found");
-		else {
-		 repository.deleteById(testId);
+	public Tests removeTest(Integer testId) {
+		Optional<Tests> test = testRepository.findById(testId);
+		if (test.isEmpty())
+			throw new TestIdNotFoundException(ERROR_MESSAGE);
+		testRepository.deleteById(testId);
 		return null;
 	}
-	
+
 	@Override
-	public Optional<Tests> viewTest(int testId) throws TestIdNotFoundException {
-		Optional<Tests> tes = repository.findById(testId);
-		if(tes==null)
-			throw new TestIdNotFoundException("test id not found");
-		return tes;
+	public Optional<Tests> viewTest(Integer testId) {
+		Optional<Tests> test = testRepository.findById(testId);
+		if (test.isEmpty())
+			throw new TestIdNotFoundException(ERROR_MESSAGE);
+		return test;
 
 	}
 
 	@Override
 	public List<Tests> viewTestsList() {
-		List<Tests> allTests= repository.viewTestsList();
-		return allTests;
-		}
-//	@Override
-//	public List<Test> viewTests() {
-//		List<Test> allTests = repository.viewTests();
-//		return allTests;
-//	}
-//
-//	@Override
-//	public Tests createTest(Tests tests) {
-//		
-//		return null;
-//	}
-
-	@Override
-	public Tests createTest(Tests test) {
-    // TODO Auto-generated method stub
-		return null;
+		return testRepository.findAll();
 	}
 
-	
-}
