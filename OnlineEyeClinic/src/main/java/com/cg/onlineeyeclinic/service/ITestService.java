2a555fea84ae062b8a36f2c903782ad96225fc58
manasa494 <75251411+manasa494@users.noreply.github.com>
2020-12-04 19:31:28 +0530

    Update ITestService.java

--- OnlineEyeClinic/src/main/java/com/cg/onlineeyeclinic/service/ITestService.java
+++ OnlineEyeClinic/src/main/java/com/cg/onlineeyeclinic/service/ITestService.java
@@ -3,17 +3,13 @@ package com.cg.onlineeyeclinic.service;
 import java.util.List;
 import java.util.Optional;
 
-import com.cg.onlineeyeclinic.exception.TestIdNotFoundException;
 import com.cg.onlineeyeclinic.model.Tests;
 
 	public interface ITestService {
 		Tests addTest(Tests tests);
 		Tests updateTest(Tests tests);
-		Tests deleteTest(int testId) throws TestIdNotFoundException;
-		Optional<Tests> viewTest(int testId)throws TestIdNotFoundException;
-		
+		Tests removeTest(Integer testId);
+		Optional<Tests> viewTest(Integer testId);
 		List<Tests> viewTestsList();
-//		List<Appointment> viewAppointments();
-		Tests createTest(Tests test);
 	}
 
